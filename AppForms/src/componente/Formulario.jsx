f6e881e0974c1966d156d5bd8dd80060397f38d1
Patricia-rn <I202404017@CIBERTEC.EDU.PE>
2024-08-23 02:20:46 +0000

    se ingreso informacion al componente formulario

--- AppForms/src/componente/Formulario.jsx
+++ AppForms/src/componente/Formulario.jsx
@@ -6,6 +6,10 @@ const Formulario = () => {
     const [ apellidos, setApellidos ] = useState('');
     const [ celular, setCelular ] = useState('');
     const [ lugar, setLugar ] = useState('');
+    const [ errors, setErrors] = useState({});
+
+    const regexTexto = /^[a-zA-ZÁÉÍÓÚñ\s]+$/;
+    const regexNumero = /^[0-9]{1,9}$/;
 
     const realizarEnvio = (e) => {
         e.preventDefault();
@@ -14,8 +18,12 @@ const Formulario = () => {
         if(!apellidos) newErrors.apellidos = 'Los apellidos son obligatorio';
         if(!celular) newErrors.celular = 'El celular es obligatorio';
         if(!lugar) newErrors.lugar = 'El Lugar es obligatorio';
+        if(!regexTexto.test(nombre)) {alert('El nombre no es válido')};
+        if(!regexTexto.test(apellidos)) {alert('Los apellidos no son válidos')};
+        if(!regexNumero.test(celular)) {alert('Celular no válido')};
+        if(!regexTexto.test(lugar)) {alert('El lugar ingresado no es válido')};
         setErrors(newErrors);
-        alert('Datos ingresados: { $(nombre), $(apellidos, $(celular), $(lugar) }');
+        alert("Datos ingresados: { ${nombre}, ${apellidos}, ${celular}, ${lugar} }");
     }
 
     const cambioNombre = (e)=> setNombre(e.target.value);
